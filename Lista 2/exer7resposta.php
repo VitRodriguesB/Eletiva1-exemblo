21a567c0b8be322a9027ac8b3870754d6193b599
VitRodriguesB <148454457+VitRodriguesB@users.noreply.github.com>
2025-03-07 22:07:01 -0300

    exer7 ao 10
    completar o 11

---
+++ Lista 2/exer7resposta.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+    <br>
+    <h1>Resposta do Exercício 7</h1>
+    <?php
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        try{
+            $valor1 = $_POST['temp1'];
+            $igual = ($valor1 - 32) * 5/9;
+            echo"O valor em Celsius e $igual";
+            
+        }catch(Exception $e){
+            echo $e->getMessage();
+        }
+        
+        }
+        
+
+    ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
